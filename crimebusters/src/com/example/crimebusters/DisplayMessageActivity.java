c9f6df60fc344bcdaf977968abecab61bff9f08e
Khushbu <khushbuvaishnav262@gmail.com>
2014-03-25 18:45:47 -0500

    Initial

---
+++ crimebusters/src/com/example/crimebusters/DisplayMessageActivity.java
@@ -0,0 +1,80 @@
+package com.example.crimebusters;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract.Profile;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class DisplayMessageActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_message);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	/** Called when the user clicks the createReport button */
+    public void createReport(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, Report.class);
+    	startActivity(intent);
+    }
+    
+    /** Called when the user clicks the updateProfile button */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH) public void updateProfile(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, Profile.class);
+    	startActivity(intent);
+    }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_message, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
---
+++ crimebusters/src/com/example/crimebusters/LowPriorityReport.java
@@ -0,0 +1,91 @@
+package com.example.crimebusters;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LowPriorityReport extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_low_priority_report);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+	
+	/** Called when the user clicks the submitLowPriorityReport button */
+    public void submitLowPriorityReport(View view) {
+        // Do something in response to button
+//    	Intent intent = new Intent(this, test.class);
+//    	startActivity(intent);
+    }
+	
+    /** Called when the user clicks the clickPictures button */
+    public void clickPictures(View view) {
+        // Do something in response to button
+//    	Intent intent = new Intent(this, test.class);
+//    	startActivity(intent);
+    }
+	
+    /** Called when the user clicks the createAudio button */
+    public void createAudio(View view) {
+        // Do something in response to button
+//    	Intent intent = new Intent(this, test.class);
+//    	startActivity(intent);
+    }
+	
+    /** Called when the user clicks the createVideo button */
+    public void createVideo(View view) {
+        // Do something in response to button
+//    	Intent intent = new Intent(this, test.class);
+//    	startActivity(intent);
+    }
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.low_priority_report, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.fragment_low_priority_report, container, false);
+			return rootView;
+		}
+	}
+
+}
